4a2cd3d6192072bfaa45f7746a1ad651d0a8fa06
NimishaMR <90333414+NimishaMR@users.noreply.github.com>
2023-10-09 21:02:07 +0530

    Update and rename Installations and Dependencies to Installations and Dependencies.sh

--- DPDK DOC/Installations and Dependencies.sh
+++ DPDK DOC/Installations and Dependencies.sh
@@ -11,3 +11,5 @@ apt-get -y install liblua5.3-dev
 apt-get -y install python3-pyelftools
 apt -y install libelf-dev
 apt-get -y install linux-headers-`uname -r` || apt -y install  linux-headers-generic
+
+
